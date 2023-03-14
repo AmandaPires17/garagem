6c6bb33dbb6dbc54ef120015366313b5eae31577
AmandaPires17 <nanapires255@gmail.com>
2023-03-14 14:43:32 -0300

    Criação do modelo Marca

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from garagem.models import Marca, Editora
+
+admin.site.register(Marca)
+admin.site.register(Editora)
--- garagem/models.py
+++ garagem/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Marca(models.Model):
+    nome = models.CharField(max_length=50) 
+    nacionalidade = models.CharField(max_length=50) 
+
+    def __str__(self):
+        return f"{self.nome.upper()} ({self.id})"
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    site = models.URLField(null=True, blank=True) # esse campo é opcional 
+
+    def __str__(self):
+        return self.nome
+    
