42b7b6674d5e611c63bb8ec9f7b84663ca979cab
AmandaPires17 <nanapires255@gmail.com>
2023-03-31 15:04:38 -0300

    novas alterações 31/03

--- garagem/models.py
+++ garagem/models.py
@@ -21,16 +21,23 @@ class Acessorio(models.Model):
     
 class Cor(models.Model):
      descricao = models.CharField(max_length=100)
-
+     
      def __str__(self):
         return self.descricao
      
+     class Meta:
+        verbose_name_plural = "cores"
+     
 class Veiculo(models.Model):
-    marca = models.ForeignKey('Marca', on_delete=models.CASCADE)
-    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)
-    cor = models.ForeignKey('Cor', on_delete=models.CASCADE)
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT)
     ano = models.IntegerField(null=True, default=0)
     preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, default=0)
 
     def __str__(self):
         return f"{self.marca} {self.categoria} {self.ano} {self.cor}"
+    
+    class Meta:
+        verbose_name = "veículo"
+    
