c3bacf9801957472b6208c6f322de090b61175c6
mariana-oliveiraaa <marianaoliveira9142@gmail.com>
2023-03-31 14:49:20 -0300

    Commit inicial

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
 
-from garagem.models import Marca, Categoria, Acessorios, Cor 
-
-admin.site.register(Marca)
-admin.site.register(Categoria)
-admin.site.register(Acessorios)
-admin.site.register(Cor)
+from garagem.models import
