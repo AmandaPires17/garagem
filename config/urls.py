116dd455fba07e0c60d9b85699330a3ca607a3c4
AmandaPires17 <nanapires255@gmail.com>
2023-05-09 14:22:43 -0300

    API Cor

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
 from django.urls import include, path
 
+
 from rest_framework.routers import DefaultRouter
 
-from garagem.views import MarcaViewSet, CategoriaViewSet
+
+from garagem.views import MarcaViewSet, CategoriaViewSet, CorViewSet
+
 
 router = DefaultRouter()
 router.register(r"marcas", MarcaViewSet)
 router.register(r"categorias", CategoriaViewSet)
+router.register(r"cores", CorViewSet)
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include(router.urls)),
+   path("admin/", admin.site.urls),
+   path("", include(router.urls)),
 ]
+
--- garagem/views.py
+++ garagem/views.py
@@ -1,12 +1,20 @@
 from rest_framework.viewsets import ModelViewSet
 
-from garagem.models import Marca, Categoria
-from garagem.serializers import MarcaSerializer, CategoriaSerializer
+
+from garagem.models import Marca, Categoria, Cor
+from garagem.serializers import MarcaSerializer, CategoriaSerializer, CorSerializer
+
 
 class MarcaViewSet(ModelViewSet):
-    queryset = Marca.objects.all()
-    serializer_class = MarcaSerializer
+   queryset = Marca.objects.all()
+   serializer_class = MarcaSerializer
+
 
 class CategoriaViewSet(ModelViewSet):
-    queryset = Categoria.objects.all()
-    serializer_class = CategoriaSerializer
+   queryset = Categoria.objects.all()
+   serializer_class = CategoriaSerializer
+
+
+class CorViewSet(ModelViewSet):
+   queryset = Cor.objects.all()
+   serializer_class = CorSerializer
